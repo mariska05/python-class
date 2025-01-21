832f7ca9129a0b3359bc97204343c9858fa3a3c0
Mariska Lalsingh <mariskalalsingh@Mariskas-Laptop.local>
2025-01-21 15:13:55 -0500

    Updated whenDONE.py

---
+++ whenDONE.py
@@ -0,0 +1,11 @@
+# when pushing out changes:
+# git push origin main
+#
+# When pulling in changes:
+# git pull origin main
+#
+# When done and want to save:
+# git commit -m "Done editing"
+#
+# git status:
+#token: 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
